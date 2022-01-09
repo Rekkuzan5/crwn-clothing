af1a0f5f370806f81946561c3155f7c1adf423fd
Rekkuzan5 <cstapp88@gmail.com>
2022-01-08 21:43:42 -0600

    update menu-items to enable navigation with reactRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
 
+import { Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop/hats*" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
